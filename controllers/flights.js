6cc50716068c6f1c162f75884cd53e61a33e9207
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-10-19 12:33:57 -0400

    display default departure date, view list of flights by departure date

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,13 +1,16 @@
 const Flight = require('../models/flights');
 
 function index(req, res) {
-  Flight.find({}, function(err, flights) {
+  Flight.find({}).sort('date').exec(function(err, flights) {
     res.render('flights/index', { flights });
   });
 };
 
 function newFlight(req, res) {
-  res.render('flights/new');
+  const newFlight = new Flight();
+  const dt = newFlight.departs;
+  const departsDate = dt.toISOString().slice(0, 16);
+  res.render('flights/new', { departsDate: departsDate });
 };
 
 function create(req, res) {

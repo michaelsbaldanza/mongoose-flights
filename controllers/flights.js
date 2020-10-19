c81525ab9c4f980adae72d6220b0e5aa3dada65c
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-10-18 21:39:02 -0400

    complete add flight functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,7 +8,17 @@ function newFlight(req, res) {
   res.render('flights/new');
 };
 
+function create(req, res) {
+  const flight = new Flight(req.body);
+  flight.save(function(err) {
+    if (err) return res.render('flights/new');
+    console.log(flight);
+    res.redirect('/flights/new');
+  });
+};
+
 module.exports = {
   index: index,
-  new: newFlight
+  new: newFlight,
+  create: create,
 };
